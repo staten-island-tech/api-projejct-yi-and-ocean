43e2a522aa30cf38bac818d0f11f72fa60a2458f
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2022-12-19 11:03:49 -0500

    figure out how the fuck to sort through this api oh god

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import json
 import pip._vendor.requests as requests
 
-
-input = input('enter word here omg')
+input = 'god'
+# input = input('enter word here omg')
 key = 'a1aafee4-36ef-4329-bc85-e87ff1b83aa2'
-r = requests.get(f"https://dictionaryapi.com/api/v3/references/collegiate/json/{input}?key={key}").json()
-print(r)
+
+def getWord(search, key):
+    r = requests.get(f"https://dictionaryapi.com/api/v3/references/collegiate/json/{search}?key={key}").json()
+    return r
+
+print(getWord(input, key))
+
+for index in getWord(input, key):
+    print(index)
