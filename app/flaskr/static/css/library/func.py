3db3177310c6b66d5b2eb3f5f4f8dba8dd2d2510
0ceanLin <113193142+0ceanLin@users.noreply.github.com>
2023-01-11 11:06:29 -0500

    1/11

--- app/flaskr/static/css/library/func.py
+++ app/flaskr/static/css/library/func.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-def getWord(Search):
+def getWord(search):
     key = 'cc94a000-270c-4303-8c37-1ba4576600e6'
     data = requests.get(
          f"https://dictionaryapi.com/api/v3/references/sd4/json/{search}?key={key}").json()
